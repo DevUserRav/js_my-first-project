bd5eaa748c0c78b0250e9eac2612cfb44f20b9f0
Avliyokhulov <avliyoqulovr694@gmail.com>
2024-04-02 18:30:39 +0500

    added html and css codes

---
+++ script.js
@@ -0,0 +1,12 @@
+// arr.slice(start,end);
+// arr.splice(start,end);
+// arr.concat();
+// arr.join();
+// arr.reverce();
+// arr.at();
+// Math.abs() minuslarni o'chiradi
+// arr.entries() index va value qaytaradi
+// const movements = [200,450,-400,-3000,-650,-130,70,1300];
+// movements.forEach((value,idx) => {
+//     console.log(`${idx}:${value}`);
+// }); 3 parametr oladi value,index va array o'zi
